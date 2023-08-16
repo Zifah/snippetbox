831b8bd7a7769105ae469563a6e88089aea4b464
Hafiz Adewuyi <hoadewuyi@gmail.com>
2023-08-16 16:09:38 +0300

    Use HTTP package constant instead of literal

--- main.go
+++ main.go
@@ -18,7 +18,7 @@ func home(w http.ResponseWriter, r *http.Request) {
 
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
 	if r.Method != "POST" {
-		w.Header().Set("Allow", "POST")
+		w.Header().Set("Allow", http.MethodPost)
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 		return
 	}
