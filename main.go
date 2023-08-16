a09b53e5ba0cbf361b1e6bd62c27bad299583dbd
Hafiz Adewuyi <hoadewuyi@gmail.com>
2023-08-16 15:38:31 +0300

    Add 2 new handlers; prevent catch-all of "/"

--- main.go
+++ main.go
@@ -6,12 +6,30 @@ import (
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello from Snippetbox"))
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
+	w.Write([]byte("Hello from Snippetbox."))
+	// TODO Hafiz: Why is this a byte array and not a string?
+	// Because the Write method only accepts a byte array
+}
+
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Create a new snippet."))
+
+}
+
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("View an existing snippet."))
 }
 
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
 
 	log.Print("Starting server on :4000")
 	log.Fatal(http.ListenAndServe(":4000", mux))
