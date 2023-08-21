4e179231cce31f5d12cbc5c033b9b9c4847721ad
Hafiz Adewuyi <hoadewuyi@gmail.com>
2023-08-21 17:28:03 +0300

    Use a base HTML template to prevent boilerplate duplication.

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -14,14 +14,19 @@ func home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	ts, err := template.ParseFiles("./ui/html/pages/home.tmpl")
+	files := []string{
+		"./ui/html/base.tmpl",
+		"./ui/html/pages/home.tmpl",
+	}
+
+	ts, err := template.ParseFiles(files...)
 	if err != nil {
 		log.Print(err.Error())
 		http.Error(w, "Internal Server Error", 500)
 		return
 	}
 
-	err = ts.Execute(w, nil)
+	err = ts.ExecuteTemplate(w, "base", nil)
 	if err != nil {
 		log.Print(err.Error())
 		http.Error(w, "Internal Server Error", 500)
