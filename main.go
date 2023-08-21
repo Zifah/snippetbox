e71cce9cd7aba8738769f9034b0c5b707f5f2690
Hafiz Adewuyi <hoadewuyi@gmail.com>
2023-08-21 15:42:48 +0300

    Restore sys-gen header

--- main.go
+++ main.go
@@ -11,7 +11,6 @@ func home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	w.Header()["Date"] = nil
 	w.Write([]byte("Hello from Snippetbox."))
 	// TODO Hafiz: Why is this a byte array and not a string?
 	// Because the Write method only accepts a byte array
