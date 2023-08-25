7f640993b6ce81d93438be02136061c38f452599
Hafiz Adewuyi <hoadewuyi@gmail.com>
2023-08-26 01:07:00 +0300

    Set up static files

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -10,6 +10,9 @@ func main() {
 	http.HandleFunc("/snippet/view", snippetView)
 	http.HandleFunc("/snippet/create", snippetCreate)
 
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	http.Handle("/static/", http.StripPrefix("/static", fileServer))
+
 	log.Print("Starting server on :4000")
 	log.Fatal(http.ListenAndServe(":4000", nil))
 }
