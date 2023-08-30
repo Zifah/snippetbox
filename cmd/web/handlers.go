1968381b88af2018008951df93698aafef0b14fb
Hafiz Adewuyi <hoadewuyi@gmail.com>
2023-08-30 16:03:52 +0300

    Use http LIB constant for HTTP POST string literal

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -38,7 +38,7 @@ func (h *home) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }
 
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
-	if r.Method != "POST" {
+	if r.Method != http.MethodPost {
 		w.Header().Set("Allow", http.MethodPost)
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 		return
