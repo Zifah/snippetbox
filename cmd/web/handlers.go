4f8f7b4534c836bac7f9be014509735e3adcc868
Hafiz Adewuyi <hoadewuyi@gmail.com>
2023-10-10 19:59:43 +0300

    Use new validator package

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -5,11 +5,10 @@ import (
 	"fmt"
 	"net/http"
 	"strconv"
-	"strings"
-	"unicode/utf8"
 
 	"github.com/julienschmidt/httprouter"
 	"snippetbox.hafiz.com.ng/internal/models"
+	"snippetbox.hafiz.com.ng/internal/validator"
 )
 
 func (a *application) home(w http.ResponseWriter, r *http.Request) {
@@ -59,10 +58,10 @@ func (a *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
 }
 
 type snippetCreateForm struct {
-	Title       string
-	Content     string
-	Expires     int
-	FieldErrors map[string]string
+	Title   string
+	Content string
+	Expires int
+	validator.Validator
 }
 
 func (a *application) snippetCreatePost(w http.ResponseWriter, r *http.Request) {
@@ -82,9 +81,10 @@ func (a *application) snippetCreatePost(w http.ResponseWriter, r *http.Request) 
 		Content: r.PostForm.Get("content"),
 		Expires: expires,
 	}
-	form.FieldErrors = validateFormFields(form)
 
-	if len(form.FieldErrors) > 0 {
+	validateFormFields(form)
+
+	if !form.Valid() {
 		data := a.newTemplateData(r)
 		data.Form = form
 		a.render(w, http.StatusUnprocessableEntity, "create.tmpl", &data)
@@ -100,21 +100,9 @@ func (a *application) snippetCreatePost(w http.ResponseWriter, r *http.Request) 
 	http.Redirect(w, r, fmt.Sprintf("/snippet/view/%d", id), http.StatusSeeOther)
 }
 
-func validateFormFields(form snippetCreateForm) map[string]string {
-	fieldErrors := make(map[string]string)
-	if strings.TrimSpace(form.Title) == "" {
-		fieldErrors["title"] = "This field cannot be blank"
-
-	} else if utf8.RuneCountInString(form.Title) > 100 {
-		fieldErrors["title"] = "This field cannot be more than 100 characters long"
-	}
-
-	if strings.TrimSpace(form.Content) == "" {
-		fieldErrors["content"] = "This field cannot be blank"
-	}
-
-	if form.Expires != 1 && form.Expires != 7 && form.Expires != 365 {
-		fieldErrors["expires"] = "This field must equal 1, 7, 365"
-	}
-	return fieldErrors
+func validateFormFields(form snippetCreateForm) {
+	form.CheckField(validator.NotBlank(form.Title), "title", "This field cannot be blank")
+	form.CheckField(validator.MaxChars(form.Title, 100), "title", "This field cannot be more than 100 characters long")
+	form.CheckField(validator.NotBlank(form.Content), "content", "This field cannot be blank")
+	form.CheckField(validator.PermittedInt(form.Expires, 1, 7, 365), "expires", "This field must equal 1, 7, 365")
 }
