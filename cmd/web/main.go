5565f378e70ede784276157b1bf68c58c6942f71
Hafiz Adewuyi <hoadewuyi@gmail.com>
2023-10-31 17:34:13 +0200

    Include database-based session manager in DI

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -8,7 +8,10 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 
+	"github.com/alexedwards/scs/mysqlstore"
+	"github.com/alexedwards/scs/v2"
 	"github.com/go-playground/form/v4"
 	_ "github.com/go-sql-driver/mysql"
 
@@ -22,11 +25,12 @@ type config struct {
 }
 
 type application struct {
-	infoLog       *log.Logger
-	errorLog      *log.Logger
-	snippets      *models.SnippetModel
-	templateCache map[string]*template.Template
-	formDecoder   *form.Decoder
+	infoLog        *log.Logger
+	errorLog       *log.Logger
+	snippets       *models.SnippetModel
+	templateCache  map[string]*template.Template
+	formDecoder    *form.Decoder
+	sessionManager *scs.SessionManager
 }
 
 func openDB(dsn string) (*sql.DB, error) {
@@ -74,7 +78,11 @@ func main() {
 
 	dec := form.NewDecoder()
 
-	app := application{infoLog, errorLog, snippets, tc, dec}
+	sessionMgr := scs.New()
+	sessionMgr.Store = mysqlstore.New(db)
+	sessionMgr.Lifetime = 12 * time.Hour
+
+	app := application{infoLog, errorLog, snippets, tc, dec, sessionMgr}
 
 	if conf.praiseAuthor {
 		infoLog.Print("Hafiz is a great author!")
