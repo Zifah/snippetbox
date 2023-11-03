cb25ff5d914f39cee8f7dc5245afabcf2f08599f
Hafiz Adewuyi <hoadewuyi@gmail.com>
2023-11-03 18:47:41 +0200

    Prevent auth-protected pages from getting cached

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -46,6 +46,8 @@ func (app *application) requireAuthentication(next http.Handler) http.Handler {
 				http.Redirect(w, r, "/user/login", http.StatusSeeOther)
 				return
 			}
+
+			w.Header().Add("Cache-Control", "no-store")
 			next.ServeHTTP(w, r)
 		}()
 	})
