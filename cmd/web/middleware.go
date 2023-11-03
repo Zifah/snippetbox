e2c557fd66b30ec1f40118611c45a9141bdc153d
Hafiz Adewuyi <hoadewuyi@gmail.com>
2023-11-03 19:24:06 +0200

    Add CSRF validation to middleware

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/justinas/nosurf"
 )
 
 func secureHeaders(next http.Handler) http.Handler {
@@ -51,4 +53,14 @@ func (app *application) requireAuthentication(next http.Handler) http.Handler {
 			next.ServeHTTP(w, r)
 		}()
 	})
+}
+
+func noSurf(next http.Handler) http.Handler {
+	csrfHandler := nosurf.New(next)
+	csrfHandler.SetBaseCookie(http.Cookie{
+		HttpOnly: true,
+		Path:     "/",
+		Secure:   true,
+	})
+	return csrfHandler
 }
